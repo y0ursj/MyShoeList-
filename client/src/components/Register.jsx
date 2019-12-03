efe26b26672784d667e2127a3ef34f807f7125ad
Sujith  John <sujithjohn@Sujiths-MacBook-Pro.local>
2019-12-03 17:33:55 -0500

    saved

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -10,8 +10,6 @@ const Register = (props) => {
       <form onSubmit={props.handleRegister} >
         <p>Username:</p>
         <input name="username" type="text" value={props.formData.username} onChange={props.handleChange} />
-        <p>Email:</p>
-        <input name="email" type="text" value={props.formData.email} onChange={props.handleChange} />
         <p>Password:</p>
         <input name="password" type="password" value={props.formData.password} onChange={props.handleChange} />
         <hr/>
